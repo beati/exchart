47d3de8f829b21c0705c3ae4df89b533be095cfe
Thomas Beati <thomas.beati@gmail.com>
2019-02-06 16:40:47 +0100

    Menu test

--- budget-client/src/app/app.module.ts
+++ budget-client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
@@ -36,6 +37,7 @@ import { ShellComponent } from './components/shell/shell.component'
         // Angular material
         MatButtonModule,
         MatIconModule,
+        MatListModule,
         MatSidenavModule,
         MatToolbarModule,
     ],
