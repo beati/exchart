3c47c0f3b66c12583a8e5ec83866610053ee6aeb
Thomas Beati <thomas.beati@gmail.com>
2019-02-19 23:14:31 +0100

    Progress on category editor

--- budget-client/src/app/components/category-editor/category-editor.component.ts
+++ budget-client/src/app/components/category-editor/category-editor.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+
+import { Budget, Category, CategoryType } from '../../domain/domain'
 
 import { BudgetService } from '../../services/budget.service'
 
@@ -8,10 +10,22 @@ import { BudgetService } from '../../services/budget.service'
     styleUrls: ['./category-editor.component.scss'],
 })
 export class CategoryEditorComponent implements OnInit {
+    @Input() Budget: Budget
+
+    Categories: Category[][] = new Array<Category[]>(CategoryType.CategoryTypeCount)
+
     constructor(
         private readonly budgetService: BudgetService,
-    ) { }
+    ) {
+        for (let i = 0; i < this.Categories.length; i++) {
+            this.Categories[i] = []
+        }
+    }
 
-    async ngOnInit(): Promise<void> {
+    ngOnInit(): void {
+        for (let i = 0; i < this.Budget.Categories.length; i++) {
+            const category = this.Budget.Categories[i]
+            this.Categories[category.Type].push(category)
+        }
     }
 }
